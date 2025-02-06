3902ef7ab998c79ee350e2c2b19095dd77ad224d
suhyeoonn <suhyeon1740@gmail.com>
2025-02-06 10:53:36 +0900

    🩹 cors 설정에 vercel 도메인 추가

--- src/main/java/com/example/bookrating/config/CorsConfig.java
+++ src/main/java/com/example/bookrating/config/CorsConfig.java
@@ -6,6 +6,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.Arrays;
+
 @Configuration
 public class CorsConfig {
     @Bean
@@ -13,7 +15,10 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true); // 서버가 응답할 때 json을 자바스크립트에 처리할 수 있게 함
-        config.addAllowedOrigin("http://localhost:3000");
+        config.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",
+                "https://book-rating-frontend.vercel.app"
+        ));
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
